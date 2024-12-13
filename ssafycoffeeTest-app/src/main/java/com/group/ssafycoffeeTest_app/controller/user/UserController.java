80a3966bd9e5efe7092122a95d1e8d89e1908e9b
imnamgi <happynj2697@gmail.com>
2024-12-13 12:08:59 +0900

    FIT : CR 저장

--- ssafycoffeeTest-app/src/main/java/com/group/ssafycoffeeTest_app/controller/user/UserController.java
+++ ssafycoffeeTest-app/src/main/java/com/group/ssafycoffeeTest_app/controller/user/UserController.java
@@ -17,18 +17,10 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
     @PostMapping("/user")  // /users/user 경로로 POST 요청 처리
     public void saveUser(@RequestBody UserCreateRequest request) {
-        System.out.println("----------------------------------------------");
-
-        System.out.println(request.getMMID()); // null
-        System.out.println(request); // com.group.ssafycoffeeTest_app.dto.user.request.UserCreateRequest@1367b0de
-        System.out.println("----------------------------------------------");
         userService.saveUser(request);  // UserService에서 사용자 생성
     }
-
-    // 사용자 목록 조회
     @GetMapping("/user")  // /users/user 경로로 GET 요청 처리
     public List<UserResponse> getUsers() {
         return userService.getUsers();  // UserService에서 모든 사용자 목록 조회
